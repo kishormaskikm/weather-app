ea75b1d6fb3890a810eff086967d00aaea90da7b
kishor-km <kishormaski16@gmail.com>
2023-05-11 16:05:09 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0a6c4fffcamshe167876f3d6154cp1d1a40jsnde7c2e06f312',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "cf33c05f0e0e6285c4c1b111489029b3";
